a6c4611b95ee12eaf66b1b164eca5354035fe20f
Kajal Tiwari <kajal.lightbulb22@gmail.com>
2019-08-31 21:33:34 +0530

    2nd update

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask,render_template,request
+import json
 import feedparser
+import urllib.parse,urllib.request
+
+
+
 
 app=Flask(__name__)
 FEED_URLS ={"whowhatwear":"http://feeds.bbci.co.uk/news/rss.xml",
@@ -8,6 +13,7 @@ FEED_URLS ={"whowhatwear":"http://feeds.bbci.co.uk/news/rss.xml",
 @app.route('/')
 def home():
     query = request.args.get("magazine")
+    weather = get_weather(request.args.get("weather"))
     if not query or query.lower() not in  FEED_URLS:
         magazine = "whowhatwear"
     else:
@@ -15,7 +21,28 @@ def home():
 
     feed = feedparser.parse(FEED_URLS[magazine])
     articles = feed["entries"]
-    return render_template('home.html',articles=articles)
+
+    return render_template('home.html',articles=articles,weather=weather)
+
+
+def get_weather(query):
+    weather_api= "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=037e412d25a08b9c27d0a1756a91b171"
+    #query = urllib.parse.quote(query)
+    weather_api=weather_api.format(query)
+    data = urllib.request.urlopen(weather_api).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {
+                "description": parsed['weather'][0]['description'],
+                "temperature": parsed['main']['temp'],
+                "city":parsed['name'],
+                "country":parsed['sys']['country'],
+
+                    }
+    return weather
+
+
 
 
 if __name__ =='__main__':
